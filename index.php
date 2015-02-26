416e73e2e0b88f2a17258204107633cde12d940f
41435842 <41435842@090027-P33S13.macklabs.br>
2015-02-26 11:35:30 -0300

    inicio do ex 4.1

--- index.php
+++ index.php
@@ -4,7 +4,28 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
-  echo '<h1>41435842</h1>';
-  echo '<h1>Leonardo  lkk</h1>';
-?>
+<!DOCTYPE html>
+ <html>
+<head>
+<meta charset="utf-8">
+        <title> exercício 4.1 </title>
+        </head>
+        <body>
+   <?php     
+        $p1="30";
+        $m1="10";
+        $m2="10";
+        $proj1="5";
+        $proj2="5";
+        $proj3="5";
+        $proj4="5";
+        $trabF="10";
+        $proc="20";
+        $pf;
+        $mi;
+        
+        mi= (30*p1+10*M1+10*M2+5*proj1+5*proj2+5*proj3+5*proj4+10*trabF+20*proc)/100
+        
+        
+        
+        
