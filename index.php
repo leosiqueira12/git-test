e8e6e686117a3ab2703c707089e136635995e7d6
Dell <Dell@Dell-PC>
2015-03-10 19:16:00 -0300

    Exercício 5.1

--- index.php
+++ index.php
@@ -11,20 +11,45 @@
         <title> exercício 4.1 </title>
         </head>
         <body>
-   <?php     
-        $p1="30";
-        $m1="10";
-        $m2="10";
-        $proj1="5";
-        $proj2="5";
-        $proj3="5";
-        $proj4="5";
-        $trabF="10";
-        $proc="20";
-        $pf;
-        $mi;
-        
-        mi= (30*p1+10*M1+10*M2+5*proj1+5*proj2+5*proj3+5*proj4+10*trabF+20*proc)/100
+   <?php
++    $i=0;
++    $info[1][1] = '02K01';
++    $info[1][2] = 'Tecnologia Web II';
++    $info[1][3] = 'Segunda';
++    $info[1][4] = 'Charles';
++    $info[2][1] = '02J01';
++    $info[2][2] = 'Linguagem de Programacao I';
++    $info[2][3] = 'Terça';
++    $info[2][4] = 'João';
++    $info[3][1] = '02K01';
++    $info[3][2] = 'Linguagem de Programacao I';
++    $info[3][3] = 'Quarta';
++    $info[3][4] = 'Charles';
++    
++    
++    for($l = 0;$l <= 3; $l++){
++        
++        if($l==0){
++            echo '<style type="text/css">
++                    table, td, th
++                    {
++                      border: 3px solid black;
++                    }
++                  </style><table><tr><td><b>Turma</b></td>
++                  <td><b>Disciplina</b></td>
++                  <td><b>Semana</b></td>
++                  <td><b>Professor</b></td></tr>';
++        }
++            echo '<tr>';
++            
++            for($j = 0;$j <= 3; $j++){
++                echo '<td>'.$info[$l][$j].'</td>';
++            }
++            echo '</tr>';
++        }
++    echo '</table>;' 
++?>
+</body>
         
         
         
